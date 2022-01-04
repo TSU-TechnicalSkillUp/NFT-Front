b342d2ff0d9e93a5ed17b5298ad1beb526c4b6c8
Henry <jeongsy@gabia.com>
2022-01-04 23:16:51 +0900

    add: 프로젝트 초기 설정

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  plugins: ['prettier'],
+  globals: {
+    context: 'readonly',
+    given: 'readonly',
+  },
+  rules: {
+    'no-console': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'vue/no-v-html': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'no-alert': 'off',
+    'vue/attribute-hyphenation': 'off',
+    'vue/require-name-property': 'off',
+    'no-shadow': 'off',
+    'import/no-cycle': 'off',
+    'no-use-before-define': 'off',
+    camelcase: 'off',
+    'no-unused-expressions': 'off',
+    'import/no-self-import': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        printWidth: 120,
+        arrowParens: 'always',
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
