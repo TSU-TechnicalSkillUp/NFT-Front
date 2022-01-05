3d01a481e1ddcaf5ad71e1322b0cf3a7dd0ebd7c
Henry <jeongsy@gabia.com>
2022-01-05 20:45:28 +0900

    fix: default pages

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>index</div>
+  <div>
+    <h1>error page</h1>
+    <p>page error currupt</p>
+    <p>page not found 404</p>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
 export default Vue.extend({
-  name: 'IndexPage',
+  name: 'ErrorPage',
 });
 </script>
 
