712796df86bc1cc4868d0a27ee926372c004a589
Henry <jeongsy@gabia.com>
2022-01-05 20:45:01 +0900

    add: default layout

---
+++ components/layoutElement/AppHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="app-header">
+    <div class="app-header__logo">Logo</div>
+    <div class="app-header__bar">bar</div>
+    <div class="app-header__console">console</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'AppHeader',
+});
+</script>
+
+<style lang="scss" scoped>
+.app-header {
+  display: flex;
+  max-width: 100%;
+  height: 72px;
+  position: sticky;
+  top: 0px;
+  z-index: 100;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 0.75rem;
+
+  &__logo {
+    display: flex;
+    height: 100%;
+    align-items: center;
+    width: 150px;
+  }
+
+  &__bar {
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    width: 500px;
+  }
+
+  &__console {
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    width: 250px;
+  }
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app-layout">
+    <AppHeader />
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import AppHeader from '~/components/layoutElement/AppHeader.vue';
+
+export default Vue.extend({
+  name: 'AppLayout',
+  components: { AppHeader },
+});
+</script>
+
+<style lang="scss" scoped>
+.app-layout {
+  display: flex;
+  min-height: 100vh;
+  background-color: rgb(255, 255, 255);
+  width: 100%;
+  flex-direction: column;
+}
+</style>
