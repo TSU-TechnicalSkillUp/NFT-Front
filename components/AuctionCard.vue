7425e9305fa32d167b839803aeff6c0e05410f7d
Henry <jeongsy@gabia.com>
2022-01-10 21:20:05 +0900

    add: AcutionCard 개발

---
+++ components/AuctionCard.vue
@@ -0,0 +1,152 @@
+<template>
+  <div class="auction-card">
+    <div class="auction-card__wrapper">
+      <div class="auction-card__wrapper--cover">
+        <div class="card-content">
+          <div class="card-content__title">{{ title }}</div>
+          <div class="card-content__body">{{ content }}</div>
+          <div class="card-content__tag">
+            <p>#2222</p>
+          </div>
+        </div>
+        <div class="card-footer">
+          <p>hohihihi</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'AuctionCard',
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    content: {
+      type: String,
+      default: '',
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.auction-card {
+  display: flex;
+  height: 100%;
+  width: 100%;
+  background-color: rgb(255, 255, 255);
+  border: 1px solid rgb(229, 232, 235);
+  border-radius: 10px;
+  position: relative;
+  z-index: 2;
+  flex-direction: column;
+
+  &__wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+    border: 1px solid #e9ebee;
+    border-radius: 6px;
+    box-shadow: 0 14px 12px -18px rgb(27 28 29 / 34%);
+    background-color: #fff;
+    transition: transform 0.2s ease-in;
+
+    &--cover {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      width: 100%;
+      height: 100%;
+      border: 1px solid #e9ebee;
+      border-radius: 6px;
+      box-shadow: 0 14px 12px -18px rgb(27 28 29 / 34%);
+      background-color: #fff;
+      transition: transform 0.2s ease-in;
+
+      .card-content {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 100%;
+        &__title {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          -webkit-line-clamp: 1;
+          white-space: nowrap;
+          white-space: normal;
+          max-height: 48px;
+          text-align: left;
+          word-break: break-all;
+          display: -webkit-box;
+          -webkit-line-clamp: 2;
+          -webkit-box-orient: vertical;
+          font-weight: 400;
+          line-height: 1.5;
+          letter-spacing: -0.3px;
+          font-size: 16px;
+          color: #1b1c1d;
+          display: block;
+          margin-top: 14px;
+        }
+
+        &__body {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          -webkit-line-clamp: 1;
+          white-space: nowrap;
+          white-space: normal;
+          max-height: 60.06px;
+          text-align: left;
+          word-break: break-all;
+          display: -webkit-box;
+          -webkit-line-clamp: 3;
+          -webkit-box-orient: vertical;
+          font-weight: 400;
+          line-height: 1.43;
+          letter-spacing: -0.3px;
+          font-size: 14px;
+          color: #858a8d;
+          margin-top: 4px;
+        }
+
+        &__tag {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          margin-top: 32px;
+          padding: 0 14px;
+        }
+      }
+
+      .card-footer {
+        font-weight: 400;
+        line-height: 1.5;
+        letter-spacing: -0.3px;
+        font-size: 12px;
+        color: #3e4042;
+        display: flex;
+        flex-shrink: 0;
+        justify-content: space-between;
+        margin-top: 14px;
+        padding: 11px 16px 10px;
+        border-top: 1px solid #f1f3f5;
+      }
+    }
+  }
+}
+
+.auction-card:hover {
+  box-shadow: rgb(4 17 29 / 25%) 0px 0px 8px 0px;
+  transition: all 0.1s ease 0s;
+}
+</style>
--- pages/auction.vue
+++ pages/auction.vue
@@ -2,16 +2,34 @@
   <div class="auction">
     <h1 class="auction__title">Explore Auction</h1>
     <AuctionTab />
+    <div class="auction__wrap">
+      <AuctionCard title="11" content="11" />
+      <AuctionCard
+        title="12"
+        content="1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
+      />
+      <AuctionCard />
+      <AuctionCard />
+      <AuctionCard />
+      <AuctionCard />
+      <AuctionCard />
+      <AuctionCard />
+      <AuctionCard />
+      <AuctionCard />
+      <AuctionCard />
+      <AuctionCard />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import AuctionCard from '~/components/AuctionCard.vue';
 import AuctionTab from '~/components/AuctionTab.vue';
 
 export default Vue.extend({
   name: 'AuctionPage',
-  components: { AuctionTab },
+  components: { AuctionTab, AuctionCard },
 });
 </script>
 
@@ -32,6 +50,19 @@ export default Vue.extend({
     background-color: #eee;
     min-height: 12rem;
     max-height: 12rem;
+  }
+
+  &__wrap {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+    align-items: start;
+    justify-items: center;
+    grid-gap: 3rem 3rem;
+    width: 100%;
+    max-width: 1800px;
+    margin: 0 auto;
+    position: relative;
+    padding: 0 2rem;
   }
 }
 </style>
