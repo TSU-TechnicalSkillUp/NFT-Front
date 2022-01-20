ac2c3d8d98d8024b48fa09c4e04985b3d0f91adc
Henry <jeongsy@gabia.com>
2022-01-20 21:31:26 +0900

    fix: custom vanta

---
+++ static/js/effet.js
@@ -0,0 +1,10 @@
+VANTA.BIRDS({
+  el: '#my', // element selector string or DOM object reference
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 500.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+});
